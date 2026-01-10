bb4cd68e4e7f574a0b9e1561f503c3e3be03095a
VenugopalV <venugopal.velcheru@gmail.com>
2026-01-09 22:39:06 -0600

    Updated with joystick and queue comms

--- Makefile
+++ Makefile
@@ -38,64 +38,176 @@
 #******************************************************************************
 
 PROJECT  = BluEagle
-CPP      = arm-none-eabi-g++
-CC       = arm-none-eabi-gcc
-AR       = arm-none-eabi-ar
-LD       = arm-none-eabi-ld
-OBJCOPY  = arm-none-eabi-objcopy
-SRCDIR   = source
-INCDIR   = header
-CFILE    = main.c
-BINDIR   = bin
-OBJ      = $(PROJECT).o
-LD_FILE  = $(PROJECT).ld
-SRCS= $(wildcard $(SRCDIR)/*.c)
-OBJECTS   = $(patsubst $(SRCDIR)/%.c,$(BINDIR)/%.o,$(SRCS))
-OBJS     = $(addprefix $(BINDIR)/,$(notdir $(SRCS:.c=.o)))
-BIN      = $(PROJECT)
-# Set the compiler CPU/FPU options.
-CPU=-mcpu=cortex-m4
-FPU=-mfpu=fpv4-sp-d16 -mfloat-abi=hard
-#INCLUDE FLAGS
-CINCS = -I"C:/Program Files (x86)/GNU Arm Embedded Toolchain/10-2021.10/arm-none-eabi/include/" 
-INCS = $(CINCS) -I./$(INCDIR) -I./$(SRCDIR) -I./inc
-CXXINCS += $(CINCS)
+TOOLCHAIN = arm-none-eabi-
+GCC = $(TOOLCHAIN)gcc
+CC = $(GCC)
+CXX = $(TOOLCHAIN)g++
+AS = $(TOOLCHAIN)as
+LD = $(TOOLCHAIN)ld
+OBJCOPY = $(TOOLCHAIN)objcopy
+AR = $(TOOLCHAIN)ar
+
+# GCC flags
+CFLAG = -c -O0 -g3
+OFLAG = -o
+INCLUDEFLAG = -I
+CPUFLAG = -mthumb -mcpu=cortex-m4
+WFLAG = -Wall -Wextra -Werror -Wno-unused-parameter
+FPUFLAG=-mfpu=fpv4-sp-d16 -mfloat-abi=softfp
 #-specs=rdimon.specs
-CFLAGS =-mcpu=cortex-m4       \
-    -mthumb                   \
-	-mfpu=fpv4-sp-d16         \
-    -mfloat-abi=hard          \
-    -ffunction-sections       \
+#CFLAGS =-ffunction-sections       \
     -fdata-sections           \
-    -O0                       \
-    -g3                       \
 	-specs=nosys.specs        \
-    -Wall                     \
-    -Wextra                   \
-    -Wpedantic #-Werror
-CXXFLAGS += $(CFLAGS) 
-#LINKER FLAGS
-LDFLAGS = -T $(LD_FILE) -e ResetISR -Wl,-Map=$(BINDIR)/$(PROJECT).map
-RM  = C:/cygwin/bin/rm.exe -f
+#CXXFLAGS += $(CFLAGS) 
 
-all: clean all-before $(BINDIR)/$(BIN).bin all-after
+CFLAGS = $(CPUFLAG) $(WFLAG) -Dgcc
+# Uncomment this if the application performs floating point operations
+#CFLAGS += $(FPUFLAG)
 
-$(OBJECTS): $(BINDIR)/%.o: $(SRCDIR)/%.c 
-	$(CC) -c $< -o $@ $(INCS) $(CFLAGS)
+# Additional C compiler flags to produce debugging symbols
+DEB_FLAG = -g -DDEBUG
 
-#$(BINDIR)/$(OBJ): $(OBJECTS)
-#	$(CC) -c -o $@ $(INCS) $(CFLAGS) $(LDFLAGS)
+# Intermediate directory for all *.o and other files:
+OBJDIR = obj/
+BINDIR = bin/
 
-#Program .elf
-#$(BINDIR)/$(BIN).elf: $(BINDIR)/$(OBJ) # $(LIB) # $(BINDIR)/main.o
-$(BINDIR)/$(BIN).elf: $(OBJECTS)
-	$(CC) -o $@ $^ $(INCS) $(CFLAGS) $(LDFLAGS) 
+# FreeRTOS source base directory
+FREERTOS_SRC = FreeRTOS/Source/
 
-#Binary
-$(BINDIR)/$(BIN).bin: $(BINDIR)/$(BIN).elf
-	$(OBJCOPY) -O binary $< $@
+# Directory with memory management source files
+FREERTOS_MEMMANG_SRC = $(FREERTOS_SRC)portable/MemMang/
+
+# Directory with platform specific source files
+FREERTOS_PORT_SRC = $(FREERTOS_SRC)portable/GCC/tm4c123g/
+
+# Directory with HW drivers' source files
+DRIVERS_SRC = drivers/
+DRIVERLIB_SRC = driverlib/
+
+# Directory with demo specific source (and header) files
+APP_DIR = app/
+APP_SRC = app/source/
+APP_INC = app/inc/
+
+vpath %.c $(FREERTOS_SRC)
+vpath %.c $(DRIVERS_SRC)
+vpath %.c $(DRIVERLIB_SRC)
+vpath %.c $(APP_SRC)
+
+# Object files to be linked into an application
+# Due to a large number, the .o files are arranged into logical groups:
+# The following o. files are only necessary if
+# certain options are enabled in FreeRTOSConfig.h
+FREERTOS_OBJS = croutine.o event_groups.o list.o queue.o stream_buffer.o tasks.o timers.o
+
+
+# Only one memory management .o file must be uncommented!
+#FREERTOS_MEMMANG_OBJS = heap_1.o
+#FREERTOS_MEMMANG_OBJS = heap_2.o
+#FREERTOS_MEMMANG_OBJS = heap_3.o
+FREERTOS_MEMMANG_OBJS = heap_4.o
+#FREERTOS_MEMMANG_OBJS = heap_5.o
+
+FREERTOS_PORT_OBJS = port.o
+
+DRIVERS_OBJS = fpu.o scb.o
+#Unnnecessary driver object files may be commented out
+DRIVERLIB_OBJS = cpu.o gpio.o i2c.o interrupt.o pwm.o sysctl.o systick.o
+
+APP_OBJS = main.o BSP_delay.o BSP_LCD.o nostdlib.o startup_gcc.o
+# nostdlib.o must be commented out if standard lib is going to be linked!
+#APP_OBJS += nostdlib.o
+
+# All object files specified above are prefixed the intermediate directory
+OBJS = $(addprefix $(OBJDIR), $(FREERTOS_OBJS) $(FREERTOS_MEMMANG_OBJS) \
+		$(FREERTOS_PORT_OBJS) $(DRIVERS_OBJS) $(DRIVERLIB_OBJS) $(APP_OBJS))
+
+# Definition of the linker script and final targets
+LINKER_SCRIPT = $(addprefix $(APP_DIR), BluEagle.ld)
+ELF_IMAGE = $(PROJECT).elf
+TARGET = $(PROJECT).bin
+
+# Include paths to be passed to $(CC) where necessary
+INC_FREERTOS = $(FREERTOS_SRC)include/
+INC_DRIVERS = $(DRIVERS_SRC)include/
+INC_DRIVERLIB = $(DRIVERLIB_SRC)inc/
+
+# Complete include flags to be passed to $(CC) where necessary
+INC_FLAGS = $(INCLUDEFLAG) $(INC_FREERTOS) $(INCLUDEFLAG) $(APP_INC) \
+            $(INCLUDEFLAG) $(FREERTOS_PORT_SRC) $(INCLUDEFLAG) $(INC_DRIVERS) \
+			$(INCLUDEFLAG) $(INC_DRIVERLIB) $(INCLUDEFLAG) $(DRIVERLIB_SRC)
+
+# Dependency on HW specific settings
+DEP_BSP = $(INC_DRIVERS)bsp.h
+DEP_FRTOS_CONFIG = $(APP_INC)/FreeRTOSConfig.h
+DEP_SETTINGS = $(DEP_FRTOS_CONFIG)
+
+#
+# Make rules:
+#
+all : $(TARGET)
+
+rebuild : clean all
+
+$(TARGET) : $(OBJDIR) $(BINDIR) $(ELF_IMAGE)
+	$(OBJCOPY) -O binary $(BINDIR)$(word 3,$^) $(BINDIR)$@
+
+$(OBJDIR) :
+	mkdir -p $@
+
+$(BINDIR):
+	mkdir -p $@
+
+$(ELF_IMAGE) : $(OBJS) $(LINKER_SCRIPT)
+	$(LD) -nostdlib -L $(OBJDIR) -T $(LINKER_SCRIPT) $(OBJS) $(OFLAG) $(BINDIR)$@
+
+debug : _debug_flags all
+
+debug_rebuild : _debug_flags rebuild
+
+_debug_flags :
+	$(eval CFLAGS += $(DEB_FLAG))
+# FreeRTOS core
+$(OBJDIR)%.o: %.c $(DEP_FRTOS_CONFIG)
+	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+# HW specific part, in FreeRTOS/Source/portable/GCC/tm4c123g/
+
+$(OBJDIR)port.o : $(FREERTOS_PORT_SRC)port.c $(DEP_FRTOS_CONFIG)
+	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+# Rules for all MemMang implementations are provided
+# Only one of these object files must be linked to the final target
+
+#$(OBJDIR)heap_1.o : $(FREERTOS_MEMMANG_SRC)heap_1.c $(DEP_FRTOS_CONFIG)
+#	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+#$(OBJDIR)heap_2.o : $(FREERTOS_MEMMANG_SRC)heap_2.c $(DEP_FRTOS_CONFIG)
+#	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+#$(OBJDIR)heap_3.o : $(FREERTOS_MEMMANG_SRC)heap_3.c $(DEP_FRTOS_CONFIG)
+#	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+$(OBJDIR)heap_4.o : $(FREERTOS_MEMMANG_SRC)heap_4.c $(DEP_FRTOS_CONFIG)
+	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+#$(OBJDIR)heap_5.o : $(FREERTOS_MEMMANG_SRC)heap_5.c $(DEP_FRTOS_CONFIG)
+#	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+# Drivers
+$(OBJDIR)%.o: %.c $(DEP_BSP)
+	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+#Driverlib 
+$(OBJDIR)%.o: %.c
+	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
+
+# Demo Application 
+$(OBJDIR)%.o: %.c $(DEP_SETTINGS)
+	$(CC) $(CFLAG) $(CFLAGS) $(INC_FLAGS) $< $(OFLAG) $@
 
 clean: clean-custom
-	$(RM) $(BINDIR)/*
+	$(RM) -r $(OBJDIR)
+	$(RM) -r $(BINDIR)
 
 .PHONY: all all-before all-after clean clean-custom
---
+++ app/source/BSP_delay.c
@@ -0,0 +1,177 @@
+// Runs on either the TM4C123 or MSP432 with an Educational BoosterPack MKII (BOOSTXL-EDUMKII)
+// This file contains the function prototypes for the software interface to the MKII BoosterPack.
+// This board support package (BSP) is an abstraction layer,
+//   forming a bridge between the low-level hardware and the high-level software.
+
+// Daniel and Jonathan Valvano
+// June 8, 2016
+
+/* This example accompanies the books
+   "Embedded Systems: Introduction to the MSP432 Microcontroller",
+   ISBN: 978-1512185676, Jonathan Valvano, copyright (c) 2016
+
+   "Embedded Systems: Real-Time Interfacing to the MSP432 Microcontroller",
+   ISBN: 978-1514676585, Jonathan Valvano, copyright (c) 2016
+
+   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers"
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2016
+
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2016
+
+ Copyright 2016 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+//  J1   J3               J4   J2
+// [ 1] [21]             [40] [20]
+// [ 2] [22]             [39] [19]
+// [ 3] [23]             [38] [18]
+// [ 4] [24]             [37] [17]
+// [ 5] [25]             [36] [16]
+// [ 6] [26]             [35] [15]
+// [ 7] [27]             [34] [14]
+// [ 8] [28]             [33] [13]
+// [ 9] [29]             [32] [12]
+// [10] [30]             [31] [11]
+
+// Connected pins in physical order
+// J1.1 +3.3V (power)
+// J1.2 joystick horizontal (X) (analog) {TM4C123 PB5/AIN11, MSP432 P6.0}
+// J1.3 UART from Bluetooth to LaunchPad (UART) {TM4C123 PB0, MSP432 P3.2}
+// J1.4 UART from LaunchPad to Bluetooth (UART) {TM4C123 PB1, MSP432 P3.3}
+// J1.5 joystick Select button (digital) {TM4C123 PE4, MSP432 P4.1}
+// J1.6 microphone (analog)              {TM4C123 PE5/AIN8, MSP432 P4.3}
+// J1.7 LCD SPI clock (SPI)              {TM4C123 PB4, MSP432 P1.5}
+// J1.8 ambient light (OPT3001) interrupt (digital) {TM4C123 PA5, MSP432 P4.6}
+// J1.9 ambient light (OPT3001) and temperature sensor (TMP006) I2C SCL (I2C)  {TM4C123 PA6, MSP432 P6.5}
+// J1.10 ambient light (OPT3001) and temperature sensor (TMP006) I2C SDA (I2C) {TM4C123 PA7, MSP432 P6.4}
+//--------------------------------------------------
+// J2.11 temperature sensor (TMP006) interrupt (digital) {TM4C123 PA2, MSP432 P3.6}
+// J2.12 nothing                         {TM4C123 PA3, MSP432 P5.2}
+// J2.13 LCD SPI CS (SPI)                {TM4C123 PA4, MSP432 P5.0}
+// J2.14 nothing                         {TM4C123 PB6, MSP432 P1.7}
+// J2.15 LCD SPI data (SPI)              {TM4C123 PB7, MSP432 P1.6}
+// J2.16 nothing (reset)
+// J2.17 LCD !RST (digital)              {TM4C123 PF0, MSP432 P5.7}
+// J2.18 Profile 4                       {TM4C123 PE0, MSP432 P3.0}
+// J2.19 servo PWM                       {TM4C123 PB2, MSP432 P2.5}
+// J2.20 GND (ground)
+//--------------------------------------------------
+// J3.21 +5V (power)
+// J3.22 GND (ground)
+// J3.23 accelerometer X (analog)        {TM4C123 PD0/AIN7, MSP432 P6.1}
+// J3.24 accelerometer Y (analog)        {TM4C123 PD1/AIN6, MSP432 P4.0}
+// J3.25 accelerometer Z (analog)        {TM4C123 PD2/AIN5, MSP432 P4.2}
+// J3.26 joystick vertical (Y) (analog)  {TM4C123 PD3/AIN4, MSP432 P4.4}
+// J3.27 Profile 0                       {TM4C123 PE1, MSP432 P4.5}
+// J3.28 Profile 1                       {TM4C123 PE2, MSP432 P4.7}
+// J3.29 Profile 2                       {TM4C123 PE3, MSP432 P5.4}
+// J3.30 Profile 3                       {TM4C123 PF1, MSP432 P5.5}
+//--------------------------------------------------
+// J4.31 LCD RS (digital)                {TM4C123 PF4, MSP432 P3.7}
+// J4.32 user Button2 (bottom) (digital) {TM4C123 PD7, MSP432 P3.5}
+// J4.33 user Button1 (top) (digital)    {TM4C123 PD6, MSP432 P5,1}
+// J4.34 Profile 6/gator hole switch     {TM4C123 PC7, MSP432 P2.3}
+// J4.35 nothing                         {TM4C123 PC6, MSP432 P6.7}
+// J4.36 Profile 5                       {TM4C123 PC5, MSP432 P6.6}
+// J4.37 RGB LED blue (PWM)              {TM4C123 PC4, MSP432 P5.6}
+// J4.38 RGB LED green (PWM)             {TM4C123 PB3, MSP432 P2.4}
+// J4.39 RGB LED red (jumper up) or LCD backlight (jumper down) (PWM) {TM4C123 PF3, MSP432 P2.6}
+// J4.40 buzzer (PWM)                    {TM4C123 PF2, MSP432 P2.7}
+//--------------------------------------------------
+// Connected pins in logic order
+// power and reset
+// J1.1 +3.3V (power)
+// J3.21 +5V (power)
+// J3.22 GND (ground)
+// J2.20 GND (ground)
+// J2.16 nothing (reset)
+//--------------------------------------------------
+// LCD graphics
+// J1.7 LCD SPI clock (SPI)              {TM4C123 PB4, MSP432 P1.5}
+// J2.13 LCD SPI CS (SPI)                {TM4C123 PA4, MSP432 P5.0}
+// J2.15 LCD SPI data (SPI)              {TM4C123 PB7, MSP432 P1.6}
+// J2.17 LCD !RST (digital)              {TM4C123 PF0, MSP432 P5.7}
+// J4.31 LCD RS (digital)                {TM4C123 PF4, MSP432 P3.7}
+//--------------------------------------------------
+// 3-color LED
+// J4.37 RGB LED blue (PWM)              {TM4C123 PC4, MSP432 P5.6}
+// J4.38 RGB LED green (PWM)             {TM4C123 PB3, MSP432 P2.4}
+// J4.39 RGB LED red (jumper up) or LCD backlight (jumper down) (PWM) {TM4C123 PF3, MSP432 P2.6}
+//--------------------------------------------------
+// user buttons
+// J4.32 user Button2 (bottom) (digital) {TM4C123 PD7, MSP432 P3.5}
+// J4.33 user Button1 (top) (digital)    {TM4C123 PD6, MSP432 P5.1}
+//--------------------------------------------------
+// buzzer output
+// J4.40 buzzer (PWM)                    {TM4C123 PF2, MSP432 P2.7}
+//--------------------------------------------------
+// Joystick
+// J1.5 joystick Select button (digital) {TM4C123 PE4, MSP432 P4.1}
+// J1.2 joystick horizontal (X) (analog) {TM4C123 PB5/AIN11, MSP432 P6.0}
+// J3.26 joystick vertical (Y) (analog)  {TM4C123 PD3/AIN4, MSP432 P4.4}
+//--------------------------------------------------
+// accelerometer
+// J3.23 accelerometer X (analog)        {TM4C123 PD0/AIN7, MSP432 P6.1}
+// J3.24 accelerometer Y (analog)        {TM4C123 PD1/AIN6, MSP432 P4.0}
+// J3.25 accelerometer Z (analog)        {TM4C123 PD2/AIN5, MSP432 P4.2}
+//--------------------------------------------------
+// microphone
+// J1.6 microphone (analog)              {TM4C123 PE5/AIN8, MSP432 P4.3}
+//--------------------------------------------------
+// light and temperature sensors (I2C)
+// J1.8 ambient light (OPT3001) interrupt (digital) {TM4C123 PA5, MSP432 P4.6}
+// J1.9 ambient light (OPT3001) and temperature sensor (TMP006) I2C SCL (I2C)  {TM4C123 PA6, MSP432 P6.5}
+// J1.10 ambient light (OPT3001) and temperature sensor (TMP006) I2C SDA (I2C) {TM4C123 PA7, MSP432 P6.4}
+// J2.11 temperature sensor (TMP006) interrupt (digital) {TM4C123 PA2, MSP432 P3.6}
+//--------------------------------------------------
+// Bluetooth booster
+// J1.3 UART from Bluetooth to LaunchPad (UART) {TM4C123 PB0, MSP432 P3.2}
+// J1.4 UART from LaunchPad to Bluetooth (UART) {TM4C123 PB1, MSP432 P3.3}
+//--------------------------------------------------
+// profile pins
+// J3.27 Profile 0                       {TM4C123 PE1, MSP432 P4.5}
+// J3.28 Profile 1                       {TM4C123 PE2, MSP432 P4.7}
+// J3.29 Profile 2                       {TM4C123 PE3, MSP432 P5.4}
+// J3.30 Profile 3                       {TM4C123 PF1, MSP432 P5.5}
+// J2.18 Profile 4                       {TM4C123 PE0, MSP432 P3.0}
+// J4.36 Profile 5                       {TM4C123 PC5, MSP432 P6.6}
+// J4.34 Profile 6                       {TM4C123 PC7, MSP432 P2.3}
+//--------------------------------------------------
+// unconnected pins
+// J2.12 nothing                         {TM4C123 PA3, MSP432 P5.2}
+// J2.14 nothing                         {TM4C123 PB6, MSP432 P1.7}
+// J2.19 servo PWM                       {TM4C123 PB2, MSP432 P2.5}
+// J4.35 nothing                         {TM4C123 PC6, MSP432 P6.7}
+#include <stdint.h>
+#include "BSP.h"
+// delay function from sysctl.c
+// which delays 3.3*ulCount cycles
+// ulCount=23746 => 1ms = 23746*3.3cycle/loop/80,000
+void parrotdelay(uint32_t ulCount){
+  if(ulCount == 0){}
+  __asm (  "    subs    r0, #1\n"
+      "    bne     parrotdelay\n"
+      "    bx      lr\n");
+}
+// ------------BSP_Delay1ms------------
+// Simple delay function which delays about n
+// milliseconds.
+// Inputs: n  number of 1 msec to wait
+// Outputs: none
+void BSP_Delay1ms(uint32_t n){
+  while(n){
+    //parrotdelay(23746);    // 1 msec, tuned at 80 MHz, originally part of LCD module
+    parrotdelay(14836);    // 1 msec, tuned at 50 MHz
+    n--;
+  }
+}
